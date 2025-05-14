1aa5b0692cf0761efe301471be584ba06bc0a341
3laaEldeen911 <324240231@sha.edu.eg>
2025-05-14 20:00:50 +0300

    edit

--- script.js
+++ script.js
@@ -1,75 +1,20 @@
-let number =Math.random()*6;
-number=Math.floor(number+1);
-console.log(number)
+let random = Math.floor(Math.random()*6)+1
+let random2 = Math.floor(Math.random()*6)+1
+let Image =document.querySelector(".image")
+let Image2 =document.querySelector(".image2")
+let source ="dice"+random+".svg"
+let source2 ="dice"+random2+".svg"
+let won =document.querySelector("h1")
 
-let number2 =Math.random()*6;
-number2=Math.floor(number2+1);
-console.log(number2)
+Image.setAttribute("src", source)
+Image2.setAttribute("src", source2)
 
-
-const dice = document.querySelector(".image");
-const won = document.querySelector("h1");
-const dice2 =document.querySelector(".image2")
-
-if(number == 1){
-
-dice.src="/dice-six-faces-one.svg"
-
+if(random>random2){
+    won.innerHTML="player 1 has won 🏆"
 }
-else if(number == 2) {
-    dice.src="/inverted-dice-2.svg"
+else if(random<random2){
+    won.innerHTML="player 2 has won 🏆"
 }
-
-else if(number == 3) {
-    dice.src="/inverted-dice-3.svg"
+else{
+    won.innerHTML="draw 🫣"
 }
-
-else if(number == 4) {
-    dice.src="/inverted-dice-4.svg"
-}
-
-else if(number == 5) {
-    dice.src="/inverted-dice-5.svg"
-}
-
-else if(number == 6) 
-    dice.src="/inverted-dice-6.svg"
-
-
-
-
-    if(number2 == 1){
-
-        dice2.src="/dice-six-faces-one.svg"
-        
-        }
-        else if(number2 == 2) {
-            dice2.src="/inverted-dice-2.svg"
-        }
-        
-        else if(number2 == 3) {
-            dice2.src="/inverted-dice-3.svg"
-        }
-        
-        else if(number2 == 4) {
-            dice2.src="/inverted-dice-4.svg"
-        }
-        
-        else if(number2 == 5) {
-            dice2.src="/inverted-dice-5.svg"
-        }
-        
-        else if(number2 == 6) 
-            dice2.src="/inverted-dice-6.svg"
-        
-        
-    if (number > number2) {
-            won.innerHTML="player 1 has won"
-    }
-            else if( number == number2 ){
-            won.innerHTML="draw"
-    }
-
-    else if(number < number2 )
-            won.innerHTML="player 2 has won"
-            
